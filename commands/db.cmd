7f1ee9afbc8abdfad620d3ef4fc269c10cc7c53e
Yurii Torbyk <yurii@findcanary.com>
2021-10-02 19:20:20 +0300

    Change db subcommands to be run as root, add new subcommands: create, drop, cleanup, restore and backup

--- commands/db.cmd
+++ commands/db.cmd
@@ -31,17 +31,57 @@ eval "$(
 case "${WARDEN_PARAMS[0]}" in
     connect)
         "${WARDEN_DIR}/bin/warden" env exec db \
-            mysql -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --database="${MYSQL_DATABASE}" "${WARDEN_PARAMS[@]:1}" "$@"
+            mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" --database="${WARDEN_PARAMS[1]:-${MYSQL_DATABASE}}" "$@"
+        ;;
+    create)
+        "${WARDEN_DIR}/bin/warden" env exec db \
+            mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${WARDEN_PARAMS[1]:-${MYSQL_DATABASE}}\`" "$@" \
+            && echo "'${WARDEN_PARAMS[1]:-${MYSQL_DATABASE}}' DB created if it did not exist"
+        ;;
+    drop)
+        "${WARDEN_DIR}/bin/warden" env exec -T db \
+            mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "DROP DATABASE IF EXISTS \`${WARDEN_PARAMS[1]:-${MYSQL_DATABASE}}\`" "$@" \
+            && echo "'${WARDEN_PARAMS[1]:-${MYSQL_DATABASE}}' DB dropped if it existed"
+        ;;
+    cleanup)
+        "${WARDEN_DIR}/bin/warden" env exec -T db \
+            mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "DROP DATABASE IF EXISTS \`${WARDEN_PARAMS[1]:-${MYSQL_DATABASE}}\`" \
+        && "${WARDEN_DIR}/bin/warden" env exec db \
+           mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${WARDEN_PARAMS[1]:-${MYSQL_DATABASE}}\`" \
+        && echo "'${WARDEN_PARAMS[1]:-${MYSQL_DATABASE}}' DB was re-created"
         ;;
     import)
         LC_ALL=C sed -E 's/DEFINER[ ]*=[ ]*`[^`]+`@`[^`]+`/DEFINER=CURRENT_USER/g' \
             | LC_ALL=C sed -E '/\@\@(GLOBAL\.GTID_PURGED|SESSION\.SQL_LOG_BIN)/d' \
             | "${WARDEN_DIR}/bin/warden" env exec -T db \
-            mysql -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --database="${MYSQL_DATABASE}" "${WARDEN_PARAMS[@]:1}" "$@"
+            mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" --database="${WARDEN_PARAMS[1]:-${MYSQL_DATABASE}}" "$@"
+        ;;
+    restore)
+        pv ${WARDEN_PARAMS[1]} \
+            | gunzip -c \
+            | LC_ALL=C sed -E 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/g' \
+            | LC_ALL=C sed -E 's/DEFINER[ ]*=[ ]*[^*]*PROCEDURE/PROCEDURE/g' \
+            | LC_ALL=C sed -E 's/DEFINER[ ]*=[ ]*[^*]*FUNCTION/FUNCTION/g' \
+            | LC_ALL=C sed -E 's/ROW_FORMAT=FIXED//g' \
+            | LC_ALL=C sed -E '/\@\@(GLOBAL\.GTID_PURGED|SESSION\.SQL_LOG_BIN)/d' \
+            | "${WARDEN_DIR}/bin/warden" env exec -T db \
+            mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" --database="${WARDEN_PARAMS[2]:-${MYSQL_DATABASE}}" "$@"
         ;;
     dump)
-            "${WARDEN_DIR}/bin/warden" env exec -T db \
-            mysqldump -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${MYSQL_DATABASE}" "${WARDEN_PARAMS[@]:1}" "$@"
+        "${WARDEN_DIR}/bin/warden" env exec -T db \
+            mysqldump -uroot -p"${MYSQL_ROOT_PASSWORD}" "${WARDEN_PARAMS[1]:-${MYSQL_DATABASE}}" "$@"
+        ;;
+    backup)
+        "${WARDEN_DIR}/bin/warden" env exec -T db \
+            mysqldump -uroot -p"${MYSQL_ROOT_PASSWORD}" "${WARDEN_PARAMS[2]:-${MYSQL_DATABASE}}" \
+            --single-transaction --quick --add-drop-table --routines=true \
+            | LC_ALL=C sed -E 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/g' \
+            | LC_ALL=C sed -E 's/DEFINER[ ]*=[ ]*[^*]*PROCEDURE/PROCEDURE/g' \
+            | LC_ALL=C sed -E 's/DEFINER[ ]*=[ ]*[^*]*FUNCTION/FUNCTION/g' \
+            | LC_ALL=C sed -E 's/ROW_FORMAT=FIXED//g' \
+            | LC_ALL=C sed -E '/\@\@(GLOBAL\.GTID_PURGED|SESSION\.SQL_LOG_BIN)/d' \
+            | gzip -9 --force \
+            > "${WARDEN_PARAMS[1]:-${MYSQL_DATABASE}.sql.gz}"
         ;;
     *)
         fatal "The command \"${WARDEN_PARAMS[0]}\" does not exist. Please use --help for usage."
