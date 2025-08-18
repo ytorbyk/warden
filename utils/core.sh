2c5f3c68d18ff4e47ba57863422b44335851ca83
Yurii Torbyk <yurii@findcanary.com>
2025-08-17 23:04:57 -0500

    Remove phpmyadmin from peered services when it is not enabled

--- utils/core.sh
+++ utils/core.sh
@@ -2,7 +2,10 @@
 [[ ! ${WARDEN_DIR} ]] && >&2 echo -e "\033[31mThis script is not intended to be run directly!\033[0m" && exit 1
 
 ## global service containers to be connected with the project docker network
-DOCKER_PEERED_SERVICES=("traefik" "tunnel" "mailhog" "phpmyadmin")
+DOCKER_PEERED_SERVICES=("traefik" "tunnel" "mailhog")
+if [[ "${WARDEN_PHPMYADMIN_ENABLE}" == 1 ]]; then
+  DOCKER_PEERED_SERVICES+=("phpmyadmin")
+fi
 
 ## messaging functions
 function warning {
